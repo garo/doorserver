42ade399c15bf59c842d8c821d26fe05a79ff9b3
Juho Mäkinen <juho.makinen@applifier.com>
2013-09-11 17:53:51 +0300

    Document sql model

--- testdata.sql
+++ testdata.sql
@@ -10,6 +10,21 @@
 -- replication to be used. All writes (audit logs etc)
 -- are done to a separated database.
 --
+-- Data model explained as set of rules:
+--  1) System has users
+--  1.1) User can be disabled (set enabled = 0)
+--  1.2) User has one-to-many keys
+--  1.3) User has one-to-many groups
+--
+--  2) System has keys (tokens)
+--  2.1) Token can be disabled (set enabled = 0)
+--
+--  3) User belongs to one-to-many groups
+--  3.1) Group can be disabled (set enabled = 0)
+--  3.2) Group has one-to-many doors which the group can open
+--
+--  4) System has Doors
+--
 drop database doorserver_test;
 create database doorserver_test;
 use doorserver_test;
