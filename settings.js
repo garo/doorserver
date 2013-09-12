722ed898e2705c91f47822c51eb906506191db8b
Juho Mäkinen <juho.makinen@gmail.com>
2013-09-12 18:08:09 +0300

    Add actual rfid reader to inputdevices

--- settings.js
+++ settings.js
@@ -8,6 +8,14 @@ settings.mysql = {
   database : "doorserver_test"
 };
 
+settings.inputdrivers = [
+		{
+				driver : "usbkeyboard",
+        device : "/dev/input/by-path/platform-bcm2708_usb-usb-0:1.2:1.0-event-kbd",
+        door_id : 1000
+		}
+];
+
 exports.get = function(key) {
   return settings[key];
 };
