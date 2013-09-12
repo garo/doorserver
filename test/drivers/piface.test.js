8e658771597c90980444898b116e3277ab006dcb
Juho Mäkinen <juho.makinen@applifier.com>
2013-09-12 15:33:59 +0300

    add piface stub

---
+++ test/drivers/piface.test.js
@@ -0,0 +1,11 @@
+
+var assert = require('assert');
+var sinon = require('sinon');
+var piface = require('../../lib/drivers/piface');
+
+describe('piface', function() {
+  it("should construct", function () {
+    var pf = new piface();
+  });
+
+});
