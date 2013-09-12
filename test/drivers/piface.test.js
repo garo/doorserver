d9505580dcf318add17d028167f421dc66ff4db3
Juho Mäkinen <juho.makinen@gmail.com>
2013-09-12 19:02:42 +0300

    Added very simple piface api

--- test/drivers/piface.test.js
+++ test/drivers/piface.test.js
@@ -4,8 +4,24 @@ var sinon = require('sinon');
 var piface = require('../../lib/drivers/piface');
 
 describe('piface', function() {
+		var pf;
   it("should construct", function () {
-    var pf = new piface();
+    pf = new piface();
+			assert.ok(pf);
+
+
   });
+
+		it("should be able to turn relay on and off", function(done) {
+				pf.on(1);
+				setTimeout(function() {
+						pf.off(1);
+						done();
+				}, 500);
+
+				pf.on(3);
+				pf.on(4);
+
+		});
 
 });
