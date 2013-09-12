0d58d02d8b6be38195d709988281e84a9501d775
Juho Mäkinen <juho.makinen@applifier.com>
2013-09-12 19:08:26 +0300

    indent

--- settings.js
+++ settings.js
@@ -1,30 +1,29 @@
-
 var settings = {};
 
 settings.mysql = {
-  host : "localhost",
-  user : "doorserver",
-  password : "doorserver",
-  database : "doorserver_test"
+  host:"localhost",
+  user:"doorserver",
+  password:"doorserver",
+  database:"doorserver_test"
 };
 
 settings.inputdrivers = [
-		{
-				driver : "usbkeyboard",
-        device : "/dev/input/by-path/platform-bcm2708_usb-usb-0:1.2:1.0-event-kbd",
-        door_id : 1000
-		}
+  {
+    driver:"usbkeyboard",
+    device:"/dev/input/by-path/platform-bcm2708_usb-usb-0:1.2:1.0-event-kbd",
+    door_id:1000
+  }
 ];
 
 settings.doors = {
-		"1000" : {
-				relay_pin : 1,
-				buzzer_pin : 4,
-				door_open_time : 5000,
-				buzzer_time : 2000
-		}
+  "1000":{
+    relay_pin:1,
+    buzzer_pin:4,
+    door_open_time:5000,
+    buzzer_time:2000
+  }
 };
 
-exports.get = function(key) {
+exports.get = function (key) {
   return settings[key];
 };
--- test/drivers/piface.test.js
+++ test/drivers/piface.test.js
@@ -1,27 +1,26 @@
-
 var assert = require('assert');
 var sinon = require('sinon');
 var piface = require('../../lib/drivers/piface');
 
-describe('piface', function() {
-		var pf;
+describe('piface', function () {
+  var pf;
   it("should construct", function () {
     pf = new piface();
-			assert.ok(pf);
+    assert.ok(pf);
 
 
   });
 
-		it("should be able to turn relay on and off", function(done) {
-				pf.on(1);
-				setTimeout(function() {
-						pf.off(1);
-						done();
-				}, 500);
+  it("should be able to turn relay on and off", function (done) {
+    pf.on(1);
+    setTimeout(function () {
+      pf.off(1);
+      done();
+    }, 500);
 
-				pf.on(3);
-				pf.on(4);
+    pf.on(3);
+    pf.on(4);
 
-		});
+  });
 
 });
