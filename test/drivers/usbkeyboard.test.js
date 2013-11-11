89acf3dcccf3909011ed9a8893ec929a6f2fefb3
Juho Mäkinen <juho.makinen@applifier.com>
2013-11-11 10:48:42 +0200

    Add support for type-property in input driver cfg
    This adds support for more obscure usb input devices
    which act like keyboard. Currently this means a "hexinput"
    device which gives the rfid id number as hex, which is
    then converted into a decimal number and prefixed with zeros
    to get at least 10 character long string.

--- test/drivers/usbkeyboard.test.js
+++ test/drivers/usbkeyboard.test.js
@@ -29,16 +29,44 @@ describe('usbdriver', function() {
     });
   });
 
-  describe("convertHexInput", function() {
-    it("should convert correctly when no padding is needed", function() {
-      var driver = new doorserver.drivers.usbkeyboard({
-        door_id : 1000,
-        type : "hexinput"
-      });
-
-      assert.equal("13958495", driver.convertHexInput("00D4FD5F"));
-
+  it("should support type:'hexinput' and convert token from hex to dec", function (done) {
+    var driver = new doorserver.drivers.usbkeyboard({
+      door_id : 1000,
+      type :"hexinput"
     });
+
+    driver.on('token', function (packet) {
+      assert.ok(packet.token, "0013958495");
+      assert.ok(packet.door_id, 1000);
+      done();
+    });
+
+    driver.emit('rawtoken', {
+      rawtoken : "00D4FD5F",
+      door_id : 1000
+    });
+
+  });
+
+  it("should pass rawtoken as-is to token event when no type has been set", function (done) {
+    var driver = new doorserver.drivers.usbkeyboard({
+      door_id : 1000
+    });
+
+    driver.on('token', function (packet) {
+      assert.ok(packet.token, "0013958495");
+      assert.ok(packet.door_id, 1000);
+      done();
+    });
+
+    driver.emit('rawtoken', {
+      rawtoken : "0013958495",
+      door_id : 1000
+    });
+
+  });
+
+  describe("convertHexInput", function() {
 
     it("should convert correctly when padding is needed", function() {
       var driver = new doorserver.drivers.usbkeyboard({
@@ -46,8 +74,22 @@ describe('usbdriver', function() {
         type : "hexinput"
       });
 
-      assert.equal("00064863", driver.convertHexInput("0000FD5F"));
-      assert.equal("00064863", driver.convertHexInput("FD5F"));
+      assert.equal("0013958495", driver.convertHexInput("00D4FD5F"));
+      assert.equal("0000064863", driver.convertHexInput("0000FD5F"));
+      assert.equal("0000064863", driver.convertHexInput("FD5F"));
+
+    });
+
+
+    it("should convert correctly when token is more than 10 chars", function() {
+      var driver = new doorserver.drivers.usbkeyboard({
+        door_id : 1000,
+        type : "hexinput"
+      });
+
+      assert.equal("1839525543154", driver.convertHexInput("01AC4C443CF2"));
+      assert.equal("1839525543154", driver.convertHexInput("1AC4C443CF2"));
+      assert.equal("80011907311", driver.convertHexInput("12A114D0EF"));
 
     });
 
